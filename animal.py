647a9221ebf01aba4e853b6b2ff666f30335c0bd
Michał Matczak <michalmatczak22@gmail.com>
2020-11-19 19:57:38 +0100

    changed animal commands

--- animal.py
+++ animal.py
@@ -2,9 +2,9 @@ import fire
 
 from cmd_animal.create import create
 from cmd_animal.ls import ls
-from cmd_animal.remove import remove
+#from cmd_animal.remove import remove
 from cmd_animal.restart import restart
-from cmd_animal.run import run
+from cmd_animal.start import start
 from cmd_animal.status import status
 from cmd_animal.stop import stop
 from cmd_animal.update import update
